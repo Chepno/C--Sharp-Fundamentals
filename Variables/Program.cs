601ba09a248832c59ca70c237baca70167bef9d1
Chepno <allanchepno266@gmail.com>
2024-12-07 15:34:57 +0300

    C# is fun

---
+++ Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+namespace Variables{
+    class Program{
+        static void Main(string[]args){
+            int age = 24;
+            string name = "Allan";
+            double height = 5.8;
+            bool isMale = true;
+            Console.WriteLine("My name is " + name + " , I am "+ age +" and I am " + height);
+        }
+    }
+}
